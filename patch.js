3b8879f109ffe129cf263ba31d4587c36aab65ca
Aniket <aniket@example.com>
2026-08-24 14:16:05 +0530

    Remove temporary patch scripts

--- patch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('style.css', 'utf8');
-
-// Replace body[data-page="about"] .nav-inner {...} entirely
-css = css.replace(/body\[data-page="about"\] \.nav-inner\s*\{[^}]+\}/, '');
-
-fs.writeFileSync('style.css', css);
